c3563a13594dcace3ab56d5f98deec9be208ee86
Yannik <115665549+yannikosterholzer@users.noreply.github.com>
2024-09-24 19:56:03 +0200

    Update irq_handler.s

--- kernel/irq/irq_handler.s
+++ kernel/irq/irq_handler.s
@@ -1,46 +1,23 @@
 .global irq_handler_ext
-
 .extern kprintf
 .extern read_core0_timer_pending
 .extern write_cntv_tval
 .extern read_cntv_tval
 .extern read_cntvtv
-.extern cntfrq
+.extern disable_cntv_irq
 
-.equ C0_IRQSOURCE,                0x40000060                    
+.equ C0_IRQSOURCE,        0x40000060                    
 
-.section .data
-	teststring: .asciz "Test Timer: %15x(sec)  %15d (dec)/n ________________________ /n"	
-	timer: .word 0x0
 .section .text
 
 	irq_handler_ext:
-
 			  push {lr}
 			  bl read_core0_timer_pending
 			  cmp r0, #0x8
 			  bne handle_irq_3_end
-			  ldr r1, =cntfrq
-			  ldr r1, [r1] // clear interrupt and set next timer
-			  bl write_cntv_tval                                           
-			
-			//Test
-			  ldr     r1, =timer
-			  ldr     r0, [r1]
-			  ldr     r1, =teststring
-			  mov     r2, #2
-			  push {r0}
-			  push {r0}
-			  bl kprintf
-			  pop  {r0}
-			  pop  {r0}
-			  ldr     r1, =timer
-			  add     r0, r0, #100
-			  str     r0, [r1]
-			  
-			  
-			//Testend
-				
+			  mvn r1, #0 					
+			  bl write_cntv_tval               	@ cleare Interrrupt und setze Zählwert auf max
+			  bl disable_cntv_irq			@ deaktiviere den Timerinterrupt				
 	handle_irq_3_end:		
 			  pop {lr}
 			  bx  lr
